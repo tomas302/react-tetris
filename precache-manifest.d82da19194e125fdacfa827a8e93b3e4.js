bfad4a02b97ad3f25d43c206208c9ab1d503e999
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-09 18:51:41 +0100

    Updates

---
+++ precache-manifest.d82da19194e125fdacfa827a8e93b3e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc20d8fc8d3cf52996ae",
+    "url": "/react-tetris/static/js/runtime~main.cc20d8fc.js"
+  },
+  {
+    "revision": "37085fdaba239e815a38",
+    "url": "/react-tetris/static/js/main.37085fda.chunk.js"
+  },
+  {
+    "revision": "e0d97482c17a3cadf4ff",
+    "url": "/react-tetris/static/js/1.e0d97482.chunk.js"
+  },
+  {
+    "revision": "37085fdaba239e815a38",
+    "url": "/react-tetris/static/css/main.5180a747.chunk.css"
+  },
+  {
+    "revision": "09c053d71493c7d694a4fec911c4a39c",
+    "url": "/react-tetris/index.html"
+  }
+];
